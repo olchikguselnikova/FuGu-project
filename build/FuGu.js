ff39e9af24fc2873bb71ed578b7ed1fd28742e98
Olga Guselnikova <olchikguselnikova@gmail.com>
2019-01-27 20:10:06 +0200

    Menu accordeon

--- build/FuGu.js
+++ build/FuGu.js
@@ -81,6 +81,24 @@ module.exports = __webpack_require__(331);
 
 __webpack_require__(332);
 
+$(function () {
+	var el = $('.nav_list li a');
+	$('.nav_list li:has("ul")').append('<span></span>');
+	el.click(function () {
+		var checkedElement = $(this).next(),
+		    visibleElement = $('.nav_list ul:visible');
+
+		visibleElement.stop().animate({ 'height': 'toggle' }, 500).parent().removeClass('active');
+		if (checkedElement.is('ul') && !checkedElement.is(':visible')) {
+			checkedElement.stop().animate({ 'height': 'toggle' }, 500).parent().addClass('active');
+			return false;
+		}
+		if (checkedElement.is('ul') && checkedElement.is(':visible')) {
+			return false;
+		}
+	});
+});
+
 /***/ }),
 
 /***/ 332:
--- src/FuGu/FuGu.js
+++ src/FuGu/FuGu.js
@@ -1,2 +1,19 @@
 import './FuGu.scss';
 
+$(function () {
+	var el = $('.nav_list li a');
+	$('.nav_list li:has("ul")').append('<span></span>');
+	el.click(function() {
+		var checkedElement = $(this).next(),
+			visibleElement = $('.nav_list ul:visible');
+			
+		visibleElement.stop().animate({'height':'toggle'}, 500).parent().removeClass('active');		
+		if((checkedElement.is('ul')) && (!checkedElement.is(':visible'))) {
+			checkedElement.stop().animate({'height':'toggle'}, 500).parent().addClass('active');
+			return false;
+        }	
+		if((checkedElement.is('ul')) && (checkedElement.is(':visible'))) {
+			return false;
+		}
+	});
+});
